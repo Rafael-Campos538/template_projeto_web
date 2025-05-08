b95948632dc4485800e70ca1a666cf2b8f1d1a3e
Rafael Campos <197754395+Rafael-Campos538@users.noreply.github.com>
2025-05-08 18:38:01 +0000

    chore: atualizacao do sql

--- src/migrations/init.sql
+++ src/migrations/init.sql
@@ -1,7 +1,31 @@
- -- Criaçao da tabela de autores
- CREATE TABLE autor(
+--Criaçao da tabela de autores
+CREATE TABLE IF NOT EXISTS autor(
+id SERIAL PRIMARY KEY,
+nome VARCHAR(100) NOT NULL,
+nacionalidade VARCHAR(50),
+data_nascimento DATE
+);
+
+CREATE TABLE IF NOT EXISTS livro(
   id SERIAL PRIMARY KEY,
-  nome VARCHAR(100) NOT NULL,
-  nacionalidade VARCHAR(50),
-  data_nascimento DATE
- );
+  titulo VARCHAR(200) NOT NULL,
+  ano_publicacao INTEGER NOT NULL, 
+  paginas INTEGER NOT NULL,
+  isbn VARCHAR(20) UNIQUE,
+  autor_id INTEGER NOT NULL, 
+  FOREIGN KEY (autor_id) REFERENCES autor(id)
+);
+
+CREATE TABLE IF NOT EXISTS categoria(
+  id SERIAL PRIMARY KEY,
+  NOME varchar(50) NOT NULL,
+  descricao TEXT
+);
+
+CREATE TABLE IF NOT EXISTS livro_categorias(
+  livro_id INTEGER NOT NULL,
+  categoria_id INTEGER NOT NULL,
+  PRIMARY KEY (livro_id, categoria_id),
+  FOREIGN KEY (livro_id) REFERENCES livro(id),
+  FOREIGN KEY (categoria_id) REFERENCES categoria(id)
+);
